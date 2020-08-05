7acf3f570784cb64120daf69539dcc88d106fb8a
Amir <amirreza.mansoorian2003@gmail.com>
2020-08-05 05:39:10 +0430

    making styles

--- public/js/app.js
+++ public/js/app.js
@@ -65909,8 +65909,8 @@ if (document.getElementById('example')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\project\programming\php\panel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\project\programming\php\panel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! G:\projects\zeuscms\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! G:\projects\zeuscms\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
